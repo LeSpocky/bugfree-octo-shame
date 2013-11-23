8967e215454e4c2fbce11012c949ee958ce4f5ab
Alexander Dahl <post@lespocky.de>
2013-11-23 13:12:22 +0100

    introduce tests

---
+++ test/test_create.c
@@ -0,0 +1,32 @@
+/*******************************************************************//**
+ *  @file   test_create.c
+ *
+ *  @author Alexander Dahl <post@lespocky.de>
+ *
+ *  @copyright  2013 Alexander Dahl
+ *
+ *  This file is part of bugfree-octo-shame.
+ *
+ *  _bugfree-octo-shame_ is free software: you can redistribute it
+ *  and/or modify it under the terms of the GNU General Public License
+ *  as published by the Free Software Foundation, either version 3 of
+ *  the License, or (at your option) any later version.
+ *
+ *  _bugfree-octo-shame_ is distributed in the hope that it will be
+ *  useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ *  of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Foobar. If not, see <http://www.gnu.org/licenses/>.
+ **********************************************************************/
+
+#include <stdlib.h>
+
+#include "list.h"
+
+int main( int argc, char **argv ) {
+
+//    return EXIT_SUCCESS;
+    return EXIT_FAILURE;
+}
